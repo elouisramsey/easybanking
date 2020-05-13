fc3ab504ec2d993f331d9477b88cc76b9fe00e39
elouisramsey <54337277+elouisramsey@users.noreply.github.com>
2020-05-13 19:23:10 -0200

    new changes

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const burgerToggle = () => {
-    const burger = document.querySelector('.burger');
-    const close = document.querySelector('.burger-active')
-    const nav = document.querySelector('.nav-links');
+  const burger = document.querySelector('.burger')
+  const close = document.querySelector('.burger-active')
+  const nav = document.querySelector('.nav-links')
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active')
-        close.style.display = 'block'
-        burger.style.display = 'none'
-    })
-    
-    close.addEventListener('click', () => {
-        nav.classList.toggle('nav-active')
-        close.style.display = 'none'
-        burger.style.display = 'block'
-    })
+  burger.addEventListener('click', () => {
+    close.style.display = 'block'
+    burger.style.display = 'none'
+    nav.style.display = 'block'
+  })
 
+  close.addEventListener('click', () => {
+    nav.style.display = 'none'
+    close.style.display = 'none'
+    burger.style.display = 'block'
+  })
 }
 
 burgerToggle()
